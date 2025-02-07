1ca21bfd3a0e0bae0c8777dc8c659dfe8d5196cd
supriya <angalwarsupriya@gmail.com>
2025-02-07 15:07:25 +0530

    first commit, header has successfully compleated

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import './App.css'
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+/*
+import Cards from '@/components/Cards/page';
+import './page.css'
+import Header from '@/components/Header/page'
+import Main from '@/components/Main/page';
+import AddOwnCard from '@/components/AddOwnCard/page';
+import Testimonial from '@/components/Testimonial/page';
+import  Login from '@/components/Login/page'
+import Footer from '@/components/Footer/page';
+*/
+// importing Components from Components Folder 
+import Header from './components/Header'
+
+import Button from 'react-bootstrap/Button';
+function App() {
+
+  return (
+    <>
+      <Header/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,92 @@
+import React from 'react'
+import './index.css'
+// importing Bootstrap Components 
+import { Container, Row, Col } from 'react-bootstrap';
+
+// importin icons from React-icons
+import { IoIosSearch } from "react-icons/io";
+import { MdMenu } from "react-icons/md";
+import { PiShoppingCartSimpleFill } from "react-icons/pi";
+import { FaBell } from "react-icons/fa"
+import { FaBookmark } from "react-icons/fa6";
+import { FaChevronDown } from "react-icons/fa"; 
+
+// starting writing Header Components
+
+function Header() {
+  return (
+    <header className='header-bg-con d-flex align-items-center pt-3 pb-3'>
+      <Container fluid>
+      <Row className='align-items-center header-row'>
+          <Col xs={5} md={2}>
+            <img src='/images/HobbyCue Logo.png' className='logo-img' alt="HobbyCue Logo"/>
+          </Col>
+          <Col xs={7} className='d-md-none i-con d-flex justify-content-end'>
+            <IoIosSearch className='i'/>
+            <FaBell className='i'/>
+            <MdMenu className='i'/>
+          </Col>
+          <Col md={4} lg={4} className='con2 d-none d-md-flex'>
+            <div className='search-bar-con'>
+              <input type='search' className='search-bar' placeholder="Search..."/>
+              <div className='search-icon-con d-flex justify-content-center align-items-center'>
+                <IoIosSearch style={{color:"#fff", fontSize:'16px'}}/>
+              </div>
+            </div>
+          </Col>
+          <Col md={6} lg={5} className='con3 d-none d-md-flex justify-content-between'>
+            <Row className='d-flex align-items-center justify-content-between' style={{ width: '100%' }}>
+              <Col className='d-flex align-items-center'>
+                <div className='explore-con d-flex align-items-center'>
+                  <img src='/images/Group.png' className='link-img' alt="Explore"/>
+                  <p className='link-p'>Explore</p>
+                  <FaChevronDown className='down'/>
+                </div>
+                <div className='hobbies-con d-flex align-items-center'>
+                  <img src='/images/Hobbies.png' className='link-img' alt="Hobbies"/>
+                  <p className='link-p'>Hobbies</p>
+                  <FaChevronDown className='down'/>
+                </div>
+              </Col>
+              <Col className='d-flex align-items-center justify-content-end'>
+                <FaBookmark className='link-i1'/>
+                <FaBell className='link-i2'/>
+                <PiShoppingCartSimpleFill className='link-i3'/>
+              </Col>
+              <Col>
+                <button className='sign-in-btn'>Sign In</button>
+              </Col>
+          </Row>
+         
+          </Col>
+        </Row>
+      </Container>
+    </header>
+  );
+}
+
+export default Header;
+
+
+
+          /*
+          <Row className='pop-up-links-con d-flex align-items-center justify-content-between' >
+            <Col className='explore-con d-flex align-items-center'>
+              <img src='images/Group.png' className='link-img'/>
+              <p className='link-p'>Explore</p>
+              <FaChevronDown className='down'/>
+            </Col>
+            <Col className='hobbies-con d-flex align-items-center'>
+              <img src='images/Hobbies.png' className='link-img' alt="Hobbies"/>
+              <p className='link-p'>Hobbies</p>
+              <FaChevronDown className='down'/>
+            </Col>
+          </Row>
+          <Row className='links-icons-con d-flex align-items-center justify-content-between'>
+            <Col className='d-flex justify-content-end'>
+              <FaBookmark className='link-i1'/>
+              <FaBell className='link-i2'/>
+              <PiShoppingCartSimpleFill className='link-i3'/>
+            </Col>
+          </Row>
+          <button className='sign-in-btn'>Sign In</button>*/
