cfbb079c7f0b162f23c99ca8e166931e65da8ac8
supriya <angalwarsupriya@gmail.com>
2025-02-08 20:35:23 +0530

    maked order of logins

--- src/components/LoginComponents/JoinInCompo/index.jsx
+++ src/components/LoginComponents/JoinInCompo/index.jsx
@@ -6,7 +6,23 @@ import { FaFacebook } from "react-icons/fa6";
 function JoinInCompo() {
   return (
     <div className='join-in-conatiner mt-md-4'>
-    <div className='login-con'>
+     
+    <form className='form-con order-md-3'>
+        <input type='email' className='input' placeholder='Email'/>
+        <input type='password' className='input' placeholder='Passowrd'/>
+        <p className='strenth-p'>Passowrd strengh</p>
+
+        <div className='sumit-con mt-md-2'>
+            <p className='submmit-p mb-md-1'>by continuin, your agree to our <span className='span-p'>Terms of Service</span> and <span className='span-p'>Policy</span></p>
+            <button type='submit' className='submmit-btn'>Continue</button>
+        </div>
+    </form>
+    <div className='lin-con d-flex justify-content-center mt-md-3 order-md-2'>
+        <hr style={{fontWeight:'bolder', color:'black' ,width:'30%'}}/>
+        <p className='line-p'>Or connect with</p>
+        <hr style={{fontWeight:'bolder', color:'black',width:'30%'}}/>
+    </div> 
+    <div className='login-con order-md-1' >
         <div className='box'>
             <FcGoogle className='g-i'/>
             <p className='continue-p'>Continue with Google.</p>
@@ -17,21 +33,6 @@ function JoinInCompo() {
             <p className='continue-p'>Continue with Facebook.</p>
         </div>
     </div>
-    <div className='lin-con d-flex justify-content-center mt-md-3'>
-        <hr style={{fontWeight:'bolder', color:'black' ,width:'30%'}}/>
-        <p className='line-p'>Or connect with</p>
-        <hr style={{fontWeight:'bolder', color:'black',width:'30%'}}/>
-    </div>  
-    <form className='form-con'>
-        <input type='email' className='input' placeholder='Email'/>
-        <input type='password' className='input' placeholder='Passowrd'/>
-        <p className='strenth-p'>Passowrd strengh</p>
-
-        <div className='sumit-con mt-md-2'>
-            <p className='submmit-p mb-md-1'>by continuin, your agree to our <span className='span-p'>Terms of Service</span> and <span className='span-p'>Policy</span></p>
-            <button type='submit' className='submmit-btn'>Continue</button>
-        </div>
-    </form>
 </div>
   )
 }
--- src/components/LoginComponents/SignInCompo/index.jsx
+++ src/components/LoginComponents/SignInCompo/index.jsx
@@ -6,23 +6,8 @@ import { IoMdLock } from "react-icons/io";
 function SignInCompo() {
   return (
     <div className='sign-in-conatiner mt-md-4'>
-        <div className='login-con'>
-            <div className='box'>
-                <FcGoogle className='g-i'/>
-                <p className='continue-p'>Continue with Google.</p>
-           </div>
-                  
-            <div className='box'>
-                <FaFacebook className='g-i'/>
-                <p className='continue-p'>Continue with Facebook.</p>
-            </div>
-        </div>
-        <div className='lin-con d-flex justify-content-center mt-md-3'>
-            <hr style={{fontWeight:'bolder', color:'black' ,width:'30%'}}/>
-            <p className='line-p'>Or connect with</p>
-            <hr style={{fontWeight:'bolder', color:'black',width:'30%'}}/>
-        </div>  
-        <form className='form-con'>
+        
+        <form className='form-con order-md-3'>
             <input type='email' className='input' placeholder='Email'/>
             <input type='password' className='input' placeholder='Passowrd'/>
             <p className='strenth-p'>Passowrd strengh</p>
@@ -40,7 +25,24 @@ function SignInCompo() {
                 </div>
                 <button type='submit' className='submmit-btn'>Continue</button>
             </div>
-        </form>
+          </form>
+          <div className='lin-con d-flex justify-content-center mt-md-3 order-md-2'>
+            <hr style={{fontWeight:'bolder', color:'black' ,width:'30%'}}/>
+            <p className='line-p'>Or connect with</p>
+            <hr style={{fontWeight:'bolder', color:'black',width:'30%'}}/>
+        </div>  
+          <div className='login-con order-md-1'>
+            <div className='box'>
+                <FcGoogle className='g-i'/>
+                <p className='continue-p'>Continue with Google.</p>
+           </div>
+                  
+            <div className='box'>
+                <FaFacebook className='g-i'/>
+                <p className='continue-p'>Continue with Facebook.</p>
+            </div>
+          </div>
+         
     </div>
   )
 }
